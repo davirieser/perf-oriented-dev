2580b7c9717eba0be4716bfad8ab94f9c4c2b3af
LV mit LCC <cb761226@login.lcc3.uibk.ac.at>
2024-04-15 19:26:37 +0200

    Minor change

--- larger_samples/npb_bt/perf_job.sh
+++ larger_samples/npb_bt/perf_job.sh
@@ -5,7 +5,7 @@
 # Name your job to be able to identify it later
 #SBATCH --job-name Perf 
 # Redirect output stream to this file
-#SBATCH --output=perf1.log
+#SBATCH --output=timing_no_perf.out
 # Maximum number of tasks (=processes) to start in total
 #SBATCH --ntasks=1
 # Maximum number of tasks (=processes) to start per node
@@ -13,9 +13,10 @@
 # Enforce exclusive node allocation, do not share with other jobs
 #SBATCH --exclusive
 
-/usr/bin/time perf stat -e L1-dcache-load-misses,L1-dcache-loads,L1-dcache-prefetch-misses,L1-dcache-prefetches,L1-dcache-store-misses,L1-dcache-stores ./npb_bt_b
+# /usr/bin/time perf stat -e L1-dcache-load-misses,L1-dcache-loads,L1-dcache-prefetch-misses,L1-dcache-prefetches,L1-dcache-store-misses,L1-dcache-stores ./npb_bt_b
 # /usr/bin/time perf stat -e L1-icache-load-misses,L1-icache-loads,LLC-load-misses,LLC-loads,LLC-prefetch-misses,LLC-prefetches ./npb_bt_b
 # /usr/bin/time perf stat -e LLC-store-misses,LLC-stores,branch-load-misses,branch-loads,dTLB-load-misses,dTLB-loads ./npb_bt_b
 # /usr/bin/time perf stat -e dTLB-store-misses,dTLB-stores,iTLB-load-misses,iTLB-loads,node-load-misses,node-loads ./npb_bt_b
 # /usr/bin/time perf stat -e node-prefetch-misses,node-prefetches,node-store-misses,node-stores ./npb_bt_b
+/usr/bin/time ./npb_bt_b
 
